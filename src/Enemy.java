bbc2681ab44655fd9bb48ee5ab48ea5bba6bd8f4
Richard <pxdp4r0x@gmail.com>
2017-06-06 21:33:03 -0700

    Initial commit

---
+++ src/Enemy.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+
+public class Enemy extends GameObject implements Collidable {
+
+    private final double MIN_FALL_SPEED = 1;
+    private final double MAX_FALL_SPEED = 3;
+
+    public Enemy() {
+        super(20, 20, (int)(Math.random() * Window.FRAME_WIDTH), -(int)(Math.random() * 1000));
+        yVel = MIN_FALL_SPEED;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.drawOval((int)x, (int)y, getWidth(), getHeight());
+    }
+
+    @Override
+    public void update() {
+        if (y >= Window.FRAME_HEIGHT)
+            reset();
+
+        y += yVel;
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, getWidth(), getHeight());
+    }
+
+    // temporary values
+    public void reset() {
+        setX((int)(Math.random() * Window.FRAME_WIDTH));
+        setY(-(int)(Math.random() * 250));
+        setyVel(MIN_FALL_SPEED);
+    }
+}
