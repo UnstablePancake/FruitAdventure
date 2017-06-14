b44fa5aaaee678bbdd07738c22c1617242b0e094
Richard <pxdp4r0x@gmail.com>
2017-06-13 20:03:16 -0700

    Change game name

--- src/Window.java
+++ src/Window.java
@@ -6,7 +6,7 @@ public class Window {
     public static final int FRAME_HEIGHT = 700;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Mr. Brunners Fruit Adventure 2.0");
+        JFrame frame = new JFrame("Mr. Brunner's Space Adventure");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setResizable(false);
